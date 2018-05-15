047344f344945e47d8aa64e836fec30058f9655a
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-05-15 17:16:03 +0100

    Update coinChanger method - splits 50 and 20 properly.

--- src/coinChanger.java
+++ src/coinChanger.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+
 public class coinChanger {
 
     public int coinChanger(int number) {
-        return number;
+
+        ArrayList<Integer> listOfCoins = new ArrayList<>();
+
+        while(number > 0) {
+            if (number >= 50 ) {
+                System.out.println("number - 50");
+                listOfCoins.add(50);
+                number -= 50;
+                System.out.println(listOfCoins);
+
+            }
+            while (number >= 20) {
+                System.out.println("number - 20");
+                listOfCoins.add(20);
+                number -= 20;
+                System.out.println(listOfCoins);
+                System.out.println(number);
+            }
+        }
+        System.out.println(number);
+        return listOfCoins.size();
     }
 }
+
+
--- test/MyTests.java
+++ test/MyTests.java
@@ -6,7 +6,13 @@ public class MyTests {
     @Test
     public void coinChanger() {
         coinChanger tester = new coinChanger();
-        Assert.assertEquals(5, tester.coinChanger(5));
+        Assert.assertNotNull(tester.coinChanger(95));
+        Assert.assertEquals("case for 90",3, tester.coinChanger(90));
+
+//        Assert.assertEquals("list of coins is empty at the beginning",0, coinChanger.listOfCoins.size());
+//        Assert.assertEquals("add 50 to the list when number equal or bigger to 50",[50], coinChanger.listOfCoins.size());
+
+//        System.out.println(list);
     }
 }
 
